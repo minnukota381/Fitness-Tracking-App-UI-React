e83243be1c7831d5c00c09bf1977f8b00aeea1cd
minnukota381 <minnukota381@gmail.com>
2024-05-03 22:27:15 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import CreateWorkout from './pages/CreateWorkout';
-import WorkoutList from './pages/WorkoutList';
-import Workout from './pages/Workout';
+import React from 'react';
 
-function App() {
+const Header = () => {
   return (
-    <div className="container">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/workouts/create" element={<CreateWorkout />} />
-        <Route path="/workouts" element={<WorkoutList />} />
-        <Route path="/workouts/:id" element={<Workout />} />
-      </Routes>
-    </div>
+    <header>
+      <h1>Fitness Tracker</h1>
+    </header>
   );
 }
 
-export default App;
-
+export default Header;
