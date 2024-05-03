492c85afe9f74e114225b7460e897ebcedb1010e
minnukota381 <minnukota381@gmail.com>
2024-05-03 22:32:04 +0530

    .

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-dark text-white p-4">
+      <div className="container">
+        <h1>Fitness Tracker</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +12,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
